3a5a1c9ef372d702459f115c1d7e4c2e9b7f9574
tausif92 <mtbajaria257@gmail.com>
2018-07-05 20:14:01 +0700

    First commit

---
+++ JavaBasics/src/basics1.java
@@ -0,0 +1,18 @@
+
+public class basics1 {
+
+	public static void main(String[] args) {
+		
+		//Add 2 numbers
+		int a=2;
+		int b=3;
+		int sum=a+b;
+		System.out.println("The sum is "+sum);
+		
+		//Defining objects of a class
+		basics2 obj = new basics2();
+		System.out.println(obj.MyMethod1());
+
+	}
+
+}
---
+++ JavaBasics/src/basics2.java
@@ -0,0 +1,9 @@
+
+public class basics2 {
+	
+public String MyMethod1() {
+	System.out.println("I am in MyMethod1");
+	return "Pass";
+	}
+
+}
